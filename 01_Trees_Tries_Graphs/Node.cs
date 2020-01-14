09032c818e9ec3cff57f6f9390985dda209ce294
unknown <davicram+github@gmail.com>
2020-01-14 13:13:09 -0800

    Split binary node into its own thing, better split namespaces, extensive Node printing and comparison improvements, completely gutted BinaryTree

---
+++ 01_Trees_Tries_Graphs/Node.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_Trees_Tries_Graphs.BinaryTree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T value;
+        public int height;
+
+        public Node<T> left  = null;
+        public Node<T> right = null;
+
+
+        public Node(T theValue)
+        {
+            value = theValue;
+        }
+
+
+        public override String ToString()
+        {
+            StringBuilder buffer = new StringBuilder (50);
+            BuildString (this, buffer, "", "");
+            return buffer.ToString ();
+        }
+
+        // Modified version of answer by VasiliNovikov:
+        // https://stackoverflow.com/questions/4965335/how-to-print-binary-tree-diagram
+        private static void BuildString(Node<T> currNode, StringBuilder buffer, String prefix, String childrenPrefix)
+        {
+            buffer.Append (prefix);
+            if (currNode == null)
+            {
+                buffer.Append ("*\r\n");
+                return;
+            }
+
+            buffer.Append (currNode.value.ToString ());
+            buffer.Append ("\r\n");
+
+            // Right then left subtrees (so placement when tilting head to left matches most tree examples)
+            BuildString (currNode.right, buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
+            BuildString (currNode.left, buffer, childrenPrefix + "└── ", childrenPrefix + "    ");
+        }
+
+
+        // Two nodes
+        public static bool operator ==(Node<T> n1, Node<T> n2)
+        {
+            if (n1 is null && n2 is null)
+                return true;
+            if (n1 is null ^ n2 is null)
+                return false;
+
+            return n1.value.CompareTo (n2.value) == 0;
+        }
+
+        public static bool operator !=(Node<T> n1, Node<T> n2)
+        {
+            if (n1 is null && n2 is null)
+                return false;
+            if (n1 is null ^ n2 is null)
+                return true;
+
+            return n1.value.CompareTo (n2.value) != 0;
+        }
+
+        public static bool operator <(Node<T> n1, Node<T> n2)
+        {
+            return n1.value.CompareTo (n2.value) < 0;
+        }
+
+        public static bool operator >(Node<T> n1, Node<T> n2)
+        {
+            return n1.value.CompareTo (n2.value) > 0;
+        }
+
+        public static bool operator <=(Node<T> n1, Node<T> n2)
+        {
+            return n1.value.CompareTo (n2.value) <= 0;
+        }
+
+        public static bool operator >=(Node<T> n1, Node<T> n2)
+        {
+            return n1.value.CompareTo (n2.value) >= 0;
+        }
+
+
+        // Left is Node, right is T
+        public static bool operator ==(Node<T> n1, T v2)
+        {
+            if (n1 is null)
+                return false;
+
+            return n1.value.CompareTo (v2) == 0;
+        }
+
+        public static bool operator !=(Node<T> n1, T v2)
+        {
+            if (n1 is null)
+                return true;
+
+            return n1.value.CompareTo (v2) != 0;
+        }
+
+        public static bool operator <(Node<T> n1, T v2)
+        {
+            return n1.value.CompareTo (v2) < 0;
+        }
+
+        public static bool operator >(Node<T> n1, T v2)
+        {
+            return n1.value.CompareTo (v2) > 0;
+        }
+
+        public static bool operator <=(Node<T> n1, T v2)
+        {
+            return n1.value.CompareTo (v2) <= 0;
+        }
+
+        public static bool operator >=(Node<T> n1, T v2)
+        {
+            return n1.value.CompareTo (v2) >= 0;
+        }
+
+
+        // Left is T, right is Node
+        public static bool operator ==(T v1, Node<T> n2)
+        {
+            if (n2 is null)
+                return true;
+
+            return v1.CompareTo (n2.value) == 0;
+        }
+
+        public static bool operator !=(T v1, Node<T> n2)
+        {
+            if (n2 is null)
+                return false;
+
+            return v1.CompareTo (n2.value) != 0;
+        }
+
+        public static bool operator <(T v1, Node<T> n2)
+        {
+            return v1.CompareTo (n2.value) < 0;
+        }
+
+        public static bool operator >(T v1, Node<T> n2)
+        {
+            return v1.CompareTo (n2.value) > 0;
+        }
+
+        public static bool operator <=(T v1, Node<T> n2)
+        {
+            return v1.CompareTo (n2.value) <= 0;
+        }
+
+        public static bool operator >=(T v1, Node<T> n2)
+        {
+            return v1.CompareTo (n2.value) >= 0;
+        }
+
+    }
+}
--- 01_Trees_Tries_Graphs/Program.cs
+++ 01_Trees_Tries_Graphs/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using _01_Trees_Tries_Graphs.BinaryTree;
 using Sorting;
 
 namespace _01_Trees_Tries_Graphs
@@ -13,22 +14,7 @@ namespace _01_Trees_Tries_Graphs
             Console.WindowWidth = Console.LargestWindowWidth-155;
             Console.WindowHeight = Console.LargestWindowHeight-15;
 
-            TrieExample ();
-            Console.WriteLine ();
-
-            Console.WriteLine ("=================================");
-            Console.WriteLine ();
-
-            BinaryTree<int> tree = BinaryTree_Control ();
-            //BinaryTree<int> tree = BinaryTree_WorstCaseLeft ();
-            //BinaryTree<int> tree = BinaryTree_WorstCaseRight ();
-            //BinaryTree<int> tree = BinaryTree_RandomWithDuplicates ();
-            //BinaryTree<int> tree = BinaryTree_RandomWithoutDuplicates ();
-
-            Console.WriteLine ();
-            Console.WriteLine ("value/weight/count");
-            Console.WriteLine (tree.headNode.TreeGraphToString ());
-            Console.WriteLine ();
+            BinaryTreeExample ();
 
             Console.WriteLine ("Press key to exit");
             Console.ReadKey (true);
@@ -60,74 +46,27 @@ namespace _01_Trees_Tries_Graphs
         }
 
 
-        private static BinaryTree<int> BinaryTree_Control()
+        static void BinaryTreeExample()
         {
             BinaryTree<int> tree = new BinaryTree<int> ();
 
-            //int[] treeVals = new int[] { 6, 4, 5 };
-            //int[] treeVals = new int[] { 6, 9, 7 };
-            int[] treeVals = new int[] { 5, 1, 7, 0, 3, 4 };
-            //int[] treeVals = new int[] { 5, 4, 8, 7, 9, 6 };
-            //int[] treeVals = ArrayTools.SequentialInts (24);
-            //ArrayTools.ReverseArray<int> (treeVals);
 
-
-            for (int i = 0; i < treeVals.Length; i++)
+            tree.head = new Node<int> (5)
             {
-                if (i > 0)
+                left = new Node<int> (3)
                 {
-                    Console.WriteLine ();
-                    Console.WriteLine ("value/weight/count");
-                    Console.WriteLine (tree.headNode.TreeGraphToString ());
+                    left = new Node<int> (2),
+                    right = new Node<int> (4)
+                },
+                right = new Node<int> (7)
+                {
+                    left = new Node<int> (6),
+                    right = new Node<int> (8)
                 }
-                tree.Insert (treeVals[i]);
-            }
+            };
 
-            return tree;
-        }
 
-        private static BinaryTree<int> BinaryTree_WorstCaseLeft()
-        {
-            BinaryTree<int> tree = new BinaryTree<int> ();
-
-            for (int i = 9; i >= 0; i--)
-                tree.Insert (i);
-
-            return tree;
-        }
-
-        private static BinaryTree<int> BinaryTree_WorstCaseRight()
-        {
-            BinaryTree<int> tree = new BinaryTree<int> ();
-
-            for (int i = 0; i < 10; i++)
-                tree.Insert (i);
-
-            return tree;
-        }
-
-        private static BinaryTree<int> BinaryTree_RandomWithDuplicates()
-        {
-            BinaryTree<int> tree = new BinaryTree<int> ();
-            Random rand = new Random ();
-
-            for (int i = 0; i < 15; i++)
-                tree.Insert (rand.Next (0, 10));
-
-            return tree;
-        }
-
-        private static BinaryTree<int> BinaryTree_RandomWithoutDuplicates()
-        {
-            BinaryTree<int> tree = new BinaryTree<int> ();
-
-            int[] randomInts = ArrayTools.SequentialInts (10);
-            ArrayTools.ShuffleArray<int> (randomInts);
-
-            for (int i = 0; i < randomInts.Length; i++)
-                tree.Insert (randomInts[i]);
-
-            return tree;
+            Console.WriteLine (tree);
         }
 
     }
