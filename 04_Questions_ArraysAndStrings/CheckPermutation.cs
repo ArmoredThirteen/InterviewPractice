dae7c736c8d35a9ab9196f167dc926ae5bd6c555
unknown <davicram+github@gmail.com>
2020-01-24 22:13:59 -0800

    Made each example responsible for its own printing so I jump around less, blocked in permutation check

---
+++ 04_Questions_ArraysAndStrings/CheckPermutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_Questions_ArraysAndStrings
+{
+    class CheckPermutation
+    {
+        #region Run Example
+        public static void RunExample()
+        {
+            PrintCheck ("", "");
+        }
+
+        private static void PrintCheck(string strOne, string strTwo)
+        {
+            Console.WriteLine (string.Concat ("-Are chars of [", strOne, "] a permutation of [", strTwo, "]"));
+            Console.WriteLine (IsPermutation (strOne, strTwo));
+        }
+        #endregion
+
+
+        // Given two strings, write a method to decide if one is a permutation of the other
+        // Returns true if both strings are null or empty
+        // Initialize bucket array of size equal to string character count
+        public static bool IsPermutation(string strOne, string strTwo)
+        {
+            if (string.IsNullOrEmpty (strOne) && string.IsNullOrEmpty (strTwo))
+                return true;
+
+            return true;
+        }
+
+    }
+}
--- 04_Questions_ArraysAndStrings/Program.cs
+++ 04_Questions_ArraysAndStrings/Program.cs
@@ -9,39 +9,11 @@ namespace _04_Questions_ArraysAndStrings
     {
         static void Main(string[] args)
         {
-            PrintUniqueCheck ("Fu-Bar");
-            PrintUniqueCheck ("Foo-Bar");
-            PrintUniqueCheck ("Fu-Barr");
-            PrintUniqueCheck ("FFu-Barr");
+            //IsUnique.RunExample ();
+            CheckPermutation.RunExample ();
 
             Console.WriteLine ("Press key to exit");
             Console.ReadKey (true);
-        }
-
-        private static void PrintUniqueCheck(string theStr)
-        {
-            Console.WriteLine ("-Are chars unique in Str: " + theStr);
-
-            // BitArray toggle then iterate and use (int)char as bit index
-            Console.WriteLine ("-                Indexes: " + BuildIndexesLine (theStr));
-            Console.WriteLine (IsUnique.IsStrUnique(theStr));
-
-            // Sorting char array the iterating and compare to neighbor
-            //Console.WriteLine (IsUnique.IsCharAraUniqueUsingSort(theStr.ToCharArray()));
-
-            // Brute-force double for loop
-            //Console.WriteLine ("-                Indexes: " + BuildIndexesLine (theStr));
-            //Console.WriteLine (IsUnique.IsStringUniqueUsingNaive(theStr));
-
-            Console.WriteLine ();
-        }
-
-        private static string BuildIndexesLine(string theStr)
-        {
-            StringBuilder builder = new StringBuilder ();
-            for (int i = 0; i < theStr.Length; i++)
-                builder.Append (i);
-            return builder.ToString();
         }
 
     }
