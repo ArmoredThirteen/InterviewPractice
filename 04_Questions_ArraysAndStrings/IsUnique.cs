96eb57dd6c34eafc9a4fcc5234228d59a9a6356c
unknown <davicram+github@gmail.com>
2020-01-24 20:26:15 -0800

    Initial add, minimum program outline

---
+++ 04_Questions_ArraysAndStrings/IsUnique.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_Questions_ArraysAndStrings
+{
+    static class IsUnique
+    {
+        public static bool AreCharsUnique(string theStr)
+        {
+
+
+            return true;
+        }
+
+    }
+}
---
+++ 04_Questions_ArraysAndStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_Questions_ArraysAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void PrintUniqueCheck(string theStr)
+        {
+            Console.WriteLine ("Are chars unique in Str: " + theStr);
+            Console.WriteLine (IsUnique.AreCharsUnique(theStr));
+            Console.WriteLine ();
+        }
+
+    }
+}
