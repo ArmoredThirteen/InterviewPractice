dadc4afa26ae35cf766953874a4d3de0b18e3bd0
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-02-12 21:34:47 -0800

    Renamed SimpleHelpers to Helpers, moved ArrayTools from sorting to Helpers

--- 01_Trees_Tries_Graphs/Program.cs
+++ 01_Trees_Tries_Graphs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using _01_Trees_Tries_Graphs.BinaryTree;
+using SimpleHelpers;
 using Sorting;
 
 namespace _01_Trees_Tries_Graphs
--- 04_Questions_ArraysAndStrings/RotateMatrix.cs
+++ 04_Questions_ArraysAndStrings/RotateMatrix.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Sorting;
+using SimpleHelpers;
 
 namespace _04_Questions_ArraysAndStrings
 {
