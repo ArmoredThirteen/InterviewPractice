b897ab1aa6cb039d3e01bf4c5caff4c7a9161446
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-02-13 14:18:02 -0800

    Converted IsUnique and built QuestBlank for easier copy/paste of Quest children classes

--- 04_Questions_ArraysAndStrings/Program.cs
+++ 04_Questions_ArraysAndStrings/Program.cs
@@ -11,15 +11,12 @@ namespace _04_Questions_ArraysAndStrings
     {
         static void Main(string[] args)
         {
-            Console.WindowTop = 0;
-            Console.WindowLeft = 0;
-            Console.WindowWidth = Console.LargestWindowWidth-130;
-            Console.WindowHeight = Console.LargestWindowHeight-15;
+            ProgramTools.SizeConsoleWindow ();
 
-            RunAllExamples ();
+            //RunAllExamples ();
+            new IsUnique ().RunQuest ();
 
-            Console.WriteLine ("Press key to exit");
-            Console.ReadKey (true);
+            ProgramTools.PauseForAnyKey ("Press any key to exit");
         }
 
 
---
+++ Helpers/QuestBlank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helpers
+{
+    class QuestBlank : Quest<object, object>
+    {
+        public override string Header => "Header";
+        public override string Description => "Description";
+
+
+        // Build lists that determine RunStep() data and each of their expected results.
+        protected override void BuildDatas()
+        {
+            runDatas.Add (null);
+            resultDatas.Add (null);
+        }
+
+
+        // Write description of this particular RunStep(), namely to identify the current runData.
+        protected override void StateGoals(object runData)
+        {
+            Console.WriteLine ("Processing: [" + runData + "]");
+        }
+
+        // Use runData to perform desired operation and return the result.
+        protected override object RunStep(object runData)
+        {
+            return true;
+        }
+
+
+        // True if result matches expectedResult.
+        protected override bool CompareResult(object result, object expectedResult)
+        {
+            return true;
+        }
+
+        // Write the resulting data.
+        protected override void StateResult(object result)
+        {
+            Console.WriteLine (result);
+        }
+
+
+        // Write warning of algorithm failure, result was not as expected.
+        protected override void AdmitFailure(object expectedResult)
+        {
+            Console.WriteLine ("!!! -> Result should have been " + expectedResult);
+        }
+
+    }
+}
