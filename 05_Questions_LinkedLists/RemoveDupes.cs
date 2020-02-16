40c553927b17e78de1800e41458b4745805b45fa
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-02-15 22:20:10 -0800

    Finished actual RemoveDupes logic

--- 05_Questions_LinkedLists/RemoveDupes.cs
+++ 05_Questions_LinkedLists/RemoveDupes.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static _05_Questions_LinkedLists.SingleLL;
 
 namespace _05_Questions_LinkedLists
 {
@@ -17,9 +18,12 @@ namespace _05_Questions_LinkedLists
         // Build lists that determine RunStep() data and each of their expected results.
         protected override void BuildDatas()
         {
+            AddDataPair (new SingleLL (), new SingleLL ());
             AddDataPair (new SingleLL (1, 2, 3, 4, 5), new SingleLL (1, 2, 3, 4, 5));
             AddDataPair (new SingleLL (1, 2, 1, 4, 5), new SingleLL (1, 2, 4, 5));
-            AddDataPair (new SingleLL (1, 1, 1), new SingleLL (1));
+            AddDataPair (new SingleLL (1, 1, 1, 1), new SingleLL (1));
+            AddDataPair (new SingleLL (1, 2, 3, 1, 2, 3), new SingleLL (1, 2, 3));
+            AddDataPair (new SingleLL (5, 4, 3, 3, 3, 1, 2, 5, 5), new SingleLL (5, 4, 3, 1, 2));
         }
 
         // Write description of this particular RunStep(), namely to identify the current runData.
@@ -31,14 +35,30 @@ namespace _05_Questions_LinkedLists
         // Use runData to perform desired operation and return the result.
         protected override SingleLL RunStep(SingleLL runData)
         {
-            RemoveDuplicates (runData);
+            RemoveDuplicatesWithBuffer (runData);
             return runData;
         }
 
 
-        private static void RemoveDuplicates(SingleLL theList)
+        private static void RemoveDuplicatesWithBuffer(SingleLL theList)
         {
+            Node prev = null;
+            Node curr = theList.root;
 
+            HashSet<int> foundInts = new HashSet<int> ();
+
+            while (curr != null)
+            {
+                if (foundInts.Contains (curr.val))
+                    prev.next = curr.next;
+                else
+                {
+                    foundInts.Add (curr.val);
+                    prev = curr;
+                }
+                
+                curr = curr.next;
+            }
         }
 
     }
