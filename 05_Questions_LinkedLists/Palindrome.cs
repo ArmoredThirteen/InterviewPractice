82f70d349273e51510f144c50c0eaafb53acc93e
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-02-19 14:51:04 -0800

    Filled in IsPalindrome, added extra comments because functionality is a little unintuitive despite working well, and set up several test cases

--- 05_Questions_LinkedLists/Palindrome.cs
+++ 05_Questions_LinkedLists/Palindrome.cs
@@ -16,7 +16,16 @@ namespace _05_Questions_LinkedLists
         // Build lists that determine RunStep() data and each of their expected results.
         protected override void BuildDatas()
         {
-            AddDataPair (null, false);
+            AddDataPair (new SingleLL (1),             true);
+            AddDataPair (new SingleLL (1, 2, 1),       true);
+            AddDataPair (new SingleLL (1, 2, 2, 1),    true);
+            AddDataPair (new SingleLL (1, 2, 1, 2, 1), true);
+
+            AddDataPair (new SingleLL (),                 false);
+            AddDataPair (new SingleLL (1, 2),             false);
+            AddDataPair (new SingleLL (1, 2, 2),          false);
+            AddDataPair (new SingleLL (2, 2, 2, 1),       false);
+            AddDataPair (new SingleLL (1, 2, 1, 2, 1, 2), false);
         }
 
         // Write description of this particular RunStep(), namely to identify the current runData.
@@ -37,9 +46,36 @@ namespace _05_Questions_LinkedLists
             if (list == null)
                 return false;
             if (list.root == null)
+                return false;
+
+            // Find halfway mark
+            // If length is odd, middle value ends up being skipped
+            //   Middle value can be anything so this is fine
+            int halfCount = (list.GetLength () + 1) / 2;
+            
+            SingleLL.Node middleNode = list.root;
+            for (int i = 0; i < halfCount; i++)
+                middleNode = middleNode.next;
+
+            return IsPalindromeRecurse (ref list.root, middleNode);
+        }
+
+        // Goes through currNode.next until it reaches end of list.
+        // As it recurses back, it first checks if compareTo and currNode are equal.
+        // Then it increments compareTo.
+        // Since compareTo is by ref, this means compareTo starts at the beginning and moves forward
+        //   while currNode is unwinding and effectively starts at the end and moves backward.
+        public static bool IsPalindromeRecurse(ref SingleLL.Node compareTo, SingleLL.Node currNode)
+        {
+            if (currNode == null)
                 return true;
 
-            return false;
+            if (!IsPalindromeRecurse (ref compareTo, currNode.next)
+                || compareTo.val != currNode.val)
+                return false;
+
+            compareTo = compareTo.next;
+            return true;
         }
 
     }
